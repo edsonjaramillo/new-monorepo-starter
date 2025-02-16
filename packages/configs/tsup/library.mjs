6b6c38b9fd3ba13b081cd5d07b4e9e86189dfdf6
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-16 14:17:43 -0600

    chore(configs): update tsup library configuration and exports

---
+++ packages/configs/tsup/library.mjs
@@ -0,0 +1,24 @@
+// import typings of tsup in mjs style
+/**
+ * @type {import("tsup").Options}
+ */
+export const prodLibraryConfig = {
+  outDir: 'dist',
+  format: ['esm'],
+  target: 'esnext',
+  clean: true,
+  dts: true,
+  minify: true,
+  treeshake: true,
+  sourcemap: false,
+};
+
+/**
+ * @type {import("tsup").Options}
+ */
+export const devLibraryConfig = {
+  ...prodLibraryConfig,
+  sourcemap: true,
+  minify: false,
+  treeshake: false,
+};
--- packages/example/tsup.config.mjs
+++ packages/example/tsup.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  outDir: 'dist', // Output directory
-  format: ['esm'], // Output format(s)
-  target: 'esnext',
-  clean: true, // Clean output directory before building
-  dts: true, // Generate .d.ts files
-  minify: true, // Minify output
-  sourcemap: true, // Generate sourcemaps
-  splitting: true, // Split output into chunks
-  treeshake: true, // Remove unused code
-  entry: ['src/addition/add.ts', 'src/subtraction/subtract.ts'], // Entry point(s)
+import { devLibraryConfig, prodLibraryConfig } from '@repo/configs/tsup/library';
+
+const entry = ['src/addition/add.ts', 'src/subtraction/subtract.ts'];
+
+export default defineConfig((options) => {
+  if (options.watch) {
+    return { ...devLibraryConfig, entry };
+  }
+  return { ...prodLibraryConfig, entry };
 });

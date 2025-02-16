5158f34df3a43f080102614dc3a1a81d39a069f8
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-16 14:45:17 -0600

    chore(logger): migrate build process from tsc to tsup

---
+++ packages/logger/tsup.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+import { devLibraryConfig, prodLibraryConfig } from '@repo/configs/tsup/library';
+
+const entry = ['src/logger.ts'];
+
+export default defineConfig((options) => {
+  if (options.watch) {
+    return { ...devLibraryConfig, entry };
+  }
+  return { ...prodLibraryConfig, entry };
+});

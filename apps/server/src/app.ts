dcb4f6eea56c2ae625168d41d9e092b7bf1cef73
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-16 15:14:44 -0600

    feat(server): add admin users router to app routes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,13 +2,12 @@ import { Hono } from 'hono';
 
 import { healthRouter } from './routers/health.routers';
 import { mathRouter } from './routers/math.routers';
+import { adminUsersRouter } from './routers/users.routers';
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' });
-});
-
+app.route('/admin/users', adminUsersRouter);
 app.route('/health', healthRouter);
 app.route('/math', mathRouter);
+
 export { app };

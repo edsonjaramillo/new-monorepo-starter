32a46af02e585c8ca372e80ac9e981501b1e858d
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-16 19:37:08 -0600

    chore(ui): migrate ui package to tsup build configuration

---
+++ packages/ui/tsup.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+import { devReactLibraryConfig, prodReactLibraryConfig } from '@repo/configs/tsup/react-library';
+
+const entry = ['src/lib/cn.ts', 'src/button.tsx', 'src/card.tsx'];
+
+export default defineConfig((options) => {
+  if (options.watch) {
+    return { ...devReactLibraryConfig, entry };
+  }
+  return { ...prodReactLibraryConfig, entry };
+});

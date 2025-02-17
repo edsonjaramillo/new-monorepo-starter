c77a7f8fca05bf9efba9f7009280500cf3822297
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-16 21:35:36 -0600

    chore(server): remove math routes and related Bruno collection files

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,13 +1,11 @@
 import { Hono } from 'hono';
 
 import { healthRouter } from './routers/health.routers';
-import { mathRouter } from './routers/math.routers';
 import { adminUsersRouter } from './routers/users.routers';
 
 const app = new Hono();
 
 app.route('/admin/users', adminUsersRouter);
 app.route('/health', healthRouter);
-app.route('/math', mathRouter);
 
 export { app };

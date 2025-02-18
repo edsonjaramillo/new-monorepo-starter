5a0af9facf8b7c5085ffd2115cb2cc4b54875d05
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-18 12:56:29 -0600

    chore(monorepo): remove `prep` script and update dev workflow

--- packages/database/tsup.config.mjs
+++ packages/database/tsup.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'tsup';
 
 import { devLibraryConfig, prodLibraryConfig } from '@repo/configs/tsup/library';
 
-const entry = ['src/database.client.ts', 'src/queries/index.ts', 'src/types/index.ts', 'src/seed/seed.main.ts'];
+const entry = [
+  'src/database.client.ts',
+  'src/queries/index.ts',
+  'src/types/index.ts',
+  'src/seed/seed.main.ts',
+];
 
 export default defineConfig((options) => {
   if (options.watch) {

ad2a54ce36c49b014d657fbe41399a75de49159b
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-25 01:02:11 -0600

    feat(validation): add auth form validation schemas

--- packages/validation/tsup.config.mjs
+++ packages/validation/tsup.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 import { devLibraryConfig, prodLibraryConfig } from '@repo/configs/tsup/library';
 
-const entry = ['src/env.ts'];
+const entry = ['src/env.ts', 'src/forms/auth.schema.ts'];
 
 export default defineConfig((options) => {
   if (options.watch) {

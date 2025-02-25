047e712acfe3cca43bf04a107ca855694de16977
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-25 11:15:48 -0600

    feat(auth): add user sign-out route and session middleware

--- apps/api/src/hono.d.ts
+++ apps/api/src/hono.d.ts
@@ -1,7 +1,9 @@
+import { SessionWithUser } from '@repo/database/types';
 import { PaginationVariable } from '@repo/http/paginate';
 
 declare module 'hono' {
   interface ContextVariableMap {
+    session: SessionWithUser;
     pagination: PaginationVariable;
   }
 }

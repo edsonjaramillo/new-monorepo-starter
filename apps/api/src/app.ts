94e03ff5c7aec671168a9eb2d283af3a10b7ceaf
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-25 09:52:37 -0600

    feat(auth): implement sign-up and sign-in authentication routes with password hashing

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono';
 
+import { publicAuthRouter } from './routers/auth.routers';
 import { healthRouter } from './routers/health.routers';
 import { adminUsersRouter } from './routers/users.routers';
 
 const app = new Hono();
 
 app.route('/admin/users', adminUsersRouter);
+app.route('/auth', publicAuthRouter);
 app.route('/health', healthRouter);
 
 export { app };
---
+++ apps/api/src/routers/auth.routers.ts
@@ -0,0 +1,50 @@
+import { sValidator } from '@hono/standard-validator';
+import { Hono } from 'hono';
+import { setCookie } from 'hono/cookie';
+import { uuidv7 } from 'uuidv7';
+
+import { JSend } from '@repo/http/JSend';
+import { signinSchema, signupSchema } from '@repo/validation/auth';
+
+import { Password } from '../utils/Password';
+import { cookieOptions } from '../utils/cookies';
+import { sessionsQueries, usersQueries } from '../utils/queries';
+
+export const publicAuthRouter = new Hono();
+
+publicAuthRouter.post('/sign-up', sValidator('json', signupSchema), async (c) => {
+  const body = c.req.valid('json');
+
+  const password = await Password.hash(body.password);
+
+  await usersQueries.createUser({ ...body, password });
+
+  return c.json(JSend.success(undefined, 'User created successfully'));
+});
+
+publicAuthRouter.post('/sign-in', sValidator('json', signinSchema), async (c) => {
+  const body = c.req.valid('json');
+
+  const credentials = await usersQueries.getUserCredentials(body.email);
+  if (!credentials) {
+    return c.json(JSend.error('Invalid credentials'));
+  }
+
+  const isVerified = await Password.verify(credentials.password, body.password);
+  if (!isVerified) {
+    return c.json(JSend.error('Invalid credentials'));
+  }
+
+  const uuid = uuidv7();
+  // add 1 month to current date
+  const expiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+  await sessionsQueries.createSession({ id: uuid, userId: credentials.id, expiresAt });
+
+  const session = await sessionsQueries.getSessionById(uuid);
+  if (!session) {
+    return c.json(JSend.error('Invalid credentials'));
+  }
+
+  setCookie(c, 'session', session.id, cookieOptions(false, expiresAt));
+  return c.json(JSend.success(session, 'Users fetched successfully'));
+});

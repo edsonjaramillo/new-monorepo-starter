ce973b58b17e21a62d46b34922a66deec59c5381
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-26 14:56:28 -0600

    feat(web): showcase Text and Button components on homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,32 +1,44 @@
 import { Button } from '@repo/ui/button';
+import { H1, H2, H3, H4, Label, Paragraph, Span } from '@repo/ui/text';
 
 export default function Page() {
   return (
-    <div className="w-responsive mx-auto flex gap-2">
-      <Button type="button" color="primary">
-        Primary
-      </Button>
-      <Button type="button" color="secondary">
-        Secondary
-      </Button>
-      <Button type="button" color="success">
-        Success
-      </Button>
-      <Button type="button" color="info">
-        Info
-      </Button>
-      <Button type="button" color="danger">
-        Danger
-      </Button>
-      <Button type="button" color="warning">
-        Warning
-      </Button>
-      <Button type="button" color="neutral">
-        Neutral
-      </Button>
-      <Button type="button" disabled>
-        Disabled
-      </Button>
+    <div className="w-responsive mx-auto flex flex-col gap-12 py-8">
+      <div className="flex flex-col gap-2">
+        <H1>Header 1</H1>
+        <H2>Header 2</H2>
+        <H3>Header 3</H3>
+        <H4>Header 4</H4>
+        <Span>Span</Span>
+        <Label>Label</Label>
+        <Paragraph>Paragraph</Paragraph>
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Button type="button" color="primary">
+          Primary
+        </Button>
+        <Button type="button" color="secondary">
+          Secondary
+        </Button>
+        <Button type="button" color="success">
+          Success
+        </Button>
+        <Button type="button" color="info">
+          Info
+        </Button>
+        <Button type="button" color="danger">
+          Danger
+        </Button>
+        <Button type="button" color="warning">
+          Warning
+        </Button>
+        <Button type="button" color="neutral">
+          Neutral
+        </Button>
+        <Button type="button" disabled>
+          Disabled
+        </Button>
+      </div>
     </div>
   );
 }

bd34357ae5d08e65e70f0751370e473fc5c7e548
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-27 01:25:05 -0600

    refactor(ui): reorder Card component Tailwind classes for consistency

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 
 export function Card({ title, children }: CardProps) {
   return (
-    <div className="bg-primary-light flex flex-col gap-2 rounded p-2 shadow">
+    <div className="bg-primary-light rounded shadow flex flex-col gap-2 p-2">
       <span className="text-primary">{title}</span>
       {children}
       <span className="text-primary-dark">description</span>

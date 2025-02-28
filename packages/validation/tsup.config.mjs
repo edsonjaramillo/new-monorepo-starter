d64785bb1b4a20078a7a015ab5bd30123f50ac67
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-02-28 13:07:57 -0600

    refactor(validation): rename auth schema to validation and update exports

--- packages/validation/tsup.config.mjs
+++ packages/validation/tsup.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 import { devLibraryConfig, prodLibraryConfig } from '@repo/configs/tsup/library';
 
-const entry = ['src/env.ts', 'src/forms/auth.schema.ts'];
+const entry = ['src/env.ts', 'src/forms/auth.validation.ts'];
 
 export default defineConfig((options) => {
   if (options.watch) {

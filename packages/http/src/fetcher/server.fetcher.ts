18003b605ffff19e7d2926077bd6757a466ec0ee
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-03 12:56:33 -0600

    fix(http): update server-side fetcher to correctly handle cookie retrieval

--- packages/http/src/fetcher/server.fetcher.ts
+++ packages/http/src/fetcher/server.fetcher.ts
@@ -9,9 +9,10 @@ export class ServerSideFetcher {
 
   async get<T>(path: string): Promise<T> {
     try {
+      const cookieStore = await cookies();
       const response = await fetch(this.baseUrl + path, {
         credentials: 'include',
-        headers: { Cookie: cookies().toString() },
+        headers: { Cookie: cookieStore.toString() },
       }).then((res) => res.json());
 
       return response as T;
@@ -21,12 +22,14 @@ export class ServerSideFetcher {
   }
 
   async post<T>(path: string, body: unknown): Promise<T> {
+    const cookieStore = await cookies();
     try {
       const json = await fetch(this.baseUrl + path, {
         method: 'POST',
         credentials: 'include',
         headers: {
           'Content-Type': 'application/json',
+          Cookie: cookieStore.toString(),
         },
         body: JSON.stringify(body),
       }).then((res) => res.json());

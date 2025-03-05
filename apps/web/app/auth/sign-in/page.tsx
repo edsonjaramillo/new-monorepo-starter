abc73755afa17927ad53e80a81435f21d613fc5e
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-05 13:47:39 -0600

    chore(web): remove entire web application structure

--- apps/web/app/auth/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInForm } from '../../../components/forms/SignInForm';
-
-export default function SignInPage() {
-  return (
-    <div className="mx-auto w-responsive py-8">
-      <SignInForm />
-    </div>
-  );
-}
--- apps/web/app/context/web.context.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthPopupContext } from '@repo/context/auth-popup';
-import { createSessionContext } from '@repo/context/session';
-
-export const useSession = createSessionContext();
-export const useAuthPopup = createAuthPopupContext();

70114ce7d8b6bd9881cda7a86a1db89bfaaced48
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-06 00:13:04 -0600

    refactor(http): consolidate fetcher implementation and update package exports

--- packages/http/src/fetcher.ts
+++ packages/http/src/fetcher.ts
@@ -1,6 +1,4 @@
-import { cookies } from 'next/headers';
-
-export class ServerSideFetcher {
+class Fetcher {
   private baseUrl: string;
 
   constructor(baseUrl: string) {
@@ -9,34 +7,36 @@ export class ServerSideFetcher {
 
   async get<T>(path: string): Promise<T> {
     try {
-      const cookieStore = await cookies();
       const response = await fetch(this.baseUrl + path, {
         credentials: 'include',
-        headers: { Cookie: cookieStore.toString() },
-      }).then((res) => res.json());
+      });
 
-      return response as T;
+      const responseJson = await response.json();
+
+      return responseJson;
     } catch {
       return undefined as unknown as T;
     }
   }
 
   async post<T>(path: string, body: unknown): Promise<T> {
-    const cookieStore = await cookies();
     try {
-      const json = await fetch(this.baseUrl + path, {
+      const response = await fetch(this.baseUrl + path, {
         method: 'POST',
         credentials: 'include',
         headers: {
           'Content-Type': 'application/json',
-          Cookie: cookieStore.toString(),
         },
         body: JSON.stringify(body),
-      }).then((res) => res.json());
+      });
 
-      return json as T;
+      const responseJson = await response.json();
+
+      return responseJson;
     } catch {
       return undefined as unknown as T;
     }
   }
 }
+
+export const $api = new Fetcher('http://localhost:4000');

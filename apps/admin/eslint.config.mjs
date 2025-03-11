8ab7dbb40c8ada974efe7ee030c847306ff16cc5
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-11 15:13:46 -0500

    chore(lint): update ESLint import for React library configuration

--- apps/admin/eslint.config.mjs
+++ apps/admin/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { reactLibraryConfig } from '@repo/configs/eslint/react-internal';
+import { reactLibraryConfig } from '@repo/configs/eslint/react-library';
 
 /** @type {import("eslint").Linter.Config} */
 export default reactLibraryConfig;

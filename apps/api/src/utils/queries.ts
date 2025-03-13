546025b6a9d9fa94b533d848504f38f18cfb4b08
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-13 11:18:00 -0500

    refactor(queries): remove SessionsQueries import from utils to streamline user queries

--- apps/api/src/utils/queries.ts
+++ apps/api/src/utils/queries.ts
@@ -1,6 +1,5 @@
-import { SessionsQueries, UsersQueries } from '@repo/database/queries';
+import { UsersQueries } from '@repo/database/queries';
 
 import { cacheConnection, databaseConnection } from './api.connections';
 
 export const usersQueries = new UsersQueries(databaseConnection, cacheConnection);
-export const sessionsQueries = new SessionsQueries(databaseConnection, cacheConnection);

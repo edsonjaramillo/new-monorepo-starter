21acdeb8fc5a58cb85e75a0eea557d6d03fdea72
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-16 17:03:37 -0500

    feat(admin): implement admin navigation menu and hamburger component for improved UI interaction

--- apps/admin/app/admin.context.ts
+++ apps/admin/app/admin.context.ts
@@ -1,5 +1,7 @@
 import { createAuthPopupContext } from '@repo/context/auth-popup';
+import { createMenuContext } from '@repo/context/menu';
 import { createSessionContext } from '@repo/context/session';
 
 export const useAuthPopup = createAuthPopupContext();
+export const useAdminNavigationMenu = createMenuContext();
 export const useSession = createSessionContext();
--- apps/admin/app/components/shared/navigation/desktop-navigation.tsx
+++ apps/admin/app/components/shared/navigation/desktop-navigation.tsx
@@ -2,17 +2,21 @@ import { Link } from '@tanstack/react-router';
 
 import { Logo } from '@repo/ui/logo';
 
+import { AdminHamburger } from '../../admin-hamburger';
+import { AdminMenu } from '../../admin-menu';
 import { Avatar } from './avatar';
 
 export function DesktopNavigation() {
   return (
     <nav className="shadow-base">
       <div className="mx-auto flex h-14 w-responsive items-center justify-between">
+        <AdminHamburger />
         <Link to="/">
           <Logo />
         </Link>
         <Avatar />
       </div>
+      <AdminMenu />
     </nav>
   );
 }

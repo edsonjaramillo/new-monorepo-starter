2992a59659ca2a3c05f131274fd08ae76bfef9e4
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-18 21:30:29 -0500

    feat(admin): enhance AdminMenu with navigation links and improve layout for better user experience

--- apps/admin/app/components/admin-menu.tsx
+++ apps/admin/app/components/admin-menu.tsx
@@ -1,38 +1,43 @@
+import { Link } from '@tanstack/react-router';
+
 import { BlurBackground } from '@repo/ui/blur-background';
 import { cn } from '@repo/ui/cn';
+import { Logo } from '@repo/ui/logo';
 
 import { useAdminNavigationMenu } from '../admin.context';
+import { navigationLinks } from '../utils/links';
+import { NavigationLinkItem } from './navigation-links';
 
 export function AdminMenu() {
-  const { open, setOpen } = useAdminNavigationMenu();
+  const { isOpen, close } = useAdminNavigationMenu();
   return (
     <>
       <div
         className={cn(
-          'fixed top-0 left-0 z-modal h-full w-64 rounded-tr-base rounded-br-base bg-muted p-2 transition-transform duration-base',
-          open ? 'translate-x-0' : '-translate-x-full',
+          'fixed top-0 left-0 z-modal h-full w-64 rounded-tr-base rounded-br-base bg-white p-2 transition-transform duration-base',
+          isOpen ? 'translate-x-0' : '-translate-x-full',
         )}>
-        <div className="mx-auto grid w-[95%]">
-          <AdminCloseButton />
-          <span>Hello</span>
+        <div className="mx-auto mt-2 grid w-[95%] gap-2">
+          <div className="mb-6 flex items-center justify-between">
+            <Link to="/" onClick={close}>
+              <Logo className="size-5" />
+            </Link>
+            <AdminCloseButton />
+          </div>
+          {navigationLinks.map((link) => (
+            <NavigationLinkItem key={link.href} link={link} />
+          ))}
         </div>
       </div>
-      <BlurBackground
-        active={open}
-        ariaLabel="Close admin navigation menu"
-        onClick={() => setOpen(false)}
-      />
+      <BlurBackground active={isOpen} ariaLabel="Close admin navigation menu" onClick={close} />
     </>
   );
 }
 
 function AdminCloseButton() {
-  const { setOpen } = useAdminNavigationMenu();
+  const { close } = useAdminNavigationMenu();
   return (
-    <button
-      type="button"
-      onClick={() => setOpen(false)}
-      className="mt-2 ml-auto w-fit cursor-pointer">
+    <button type="button" onClick={close} className="ml-auto w-fit cursor-pointer">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"

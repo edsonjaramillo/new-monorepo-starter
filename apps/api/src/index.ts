480062581ec095476cf3f171e763827d6b469d11
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-03-31 14:38:22 -0500

    chore(env): refactor dotenv usage in configuration files to improve consistency and clarity

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import { serve } from '@hono/node-server';
 import { Logger } from '@repo/logger';
+import { config } from 'dotenv';
 import { app } from './app';
 import { apiEnv } from './utils/api.env';
-import 'dotenv/config';
+
+config();
 
 Logger.info(`Listening on port ${apiEnv.PORT}`);
 serve({ fetch: app.fetch, port: apiEnv.PORT });
--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,15 +1,17 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-import { databaseEnv } from './src/utils/database.env';
-import 'dotenv/config';
 
+config();
+
+const env = process.env;
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
-    host: databaseEnv.POSTGRES_HOST,
-    port: databaseEnv.POSTGRES_PORT,
-    user: databaseEnv.POSTGRES_USER,
-    password: databaseEnv.POSTGRES_PASSWORD,
-    database: databaseEnv.POSTGRES_DATABASE,
+    host: env.POSTGRES_HOST as string,
+    port: Number.parseInt(env.POSTGRES_PORT as string),
+    user: env.POSTGRES_USER as string,
+    password: env.POSTGRES_PASSWORD as string,
+    database: env.POSTGRES_DATABASE as string,
   },
   schema: './src/schema/*.schema.ts',
   casing: 'snake_case',
